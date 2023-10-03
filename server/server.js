4952cbd8a55f5f8c5a8a73581ecc2dcc41e1cd5a
M. Enes Celik <m96celik@gmail.com>
2023-10-03 20:25:08 +0300

    build-folder-added

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ mongoose
     .catch((err) => console.error(err));
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: [""],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 })
