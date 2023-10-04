39cd61ec130b27b2d7e22cec06f89d917bdc6f6f
M. Enes Celik <m96celik@gmail.com>
2023-10-04 16:18:00 +0300

    index.js-updated

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
-import disableReactDevTools from '@fvilers/disable-react-devtools';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 if(process.env.NODE_ENV === 'production') disableReactDevTools(); 
 
